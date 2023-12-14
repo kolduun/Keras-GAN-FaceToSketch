820b526633bee894dd783947d93be69deb4c40d7
kolduun <153863649+kolduun@users.noreply.github.com>
2023-12-15 01:08:24 +0500

    to fix mistake

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from backend.cliobot import run_clio_telegram_bot
 
 if __name__ == "__main__":
-    bot1 = run_clio_telegram_bot()
-    bot2 = run_clio_telegram_bot()
-    bot3 = run_clio_telegram_bot()
-    print(bot1==bot2==bot3)
+    run_clio_telegram_bot()
